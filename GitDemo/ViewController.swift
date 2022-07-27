f656b11afa5c56a03245e255278c6a69d84cb676
igor.dikan <dikan@technokratos.com>
2022-07-27 18:01:45 +0300

    Добавлена кнопка

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    let button = UIButton(type: .system)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(button)
+        button.frame = CGRect(x: 100, y: 200, width: 100, height: 50)
+        button.setTitle("Title", for: .normal)
     }
-
-
 }
-
